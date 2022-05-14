0a0274398717ec58ad9cd7762e4843d22e452b6e
joelfreem <joelfreem@gmail.com>
2022-05-14 14:10:17 -0400

    commit

--- src/company/Git.java
+++ src/company/Git.java
@@ -2,11 +2,16 @@ package company;
 
 public class Git {
     public static void main(String[] args) {
+
+        //write your code here
+        //team lead project
         System.out.println("I am Groot");
         System.out.println("I am Groot2");
         System.out.println("I am Groot3");
 
         System.out.println("I am correct code");
         System.out.println("I am not confused");
+        System.out.println("Hello my friend");
+
     }
 }
